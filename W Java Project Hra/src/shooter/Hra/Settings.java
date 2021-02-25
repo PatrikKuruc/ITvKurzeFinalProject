983d63e0034a2f24f487d5feb2697613cfa329d6
Tomáš Šalapa <74919389+TomasSalapa@users.noreply.github.com>
2021-02-25 12:08:51 +0100

    Update Settings.java

--- W Java Project Hra/src/shooter/Hra/Settings.java
+++ W Java Project Hra/src/shooter/Hra/Settings.java
@@ -13,7 +13,7 @@ public class Settings {
 	public static final Dimension PANEL_PLATNO_SIZE = new Dimension(WINDOW_HEIGHT, WINDOW_WIDTH);
 	public static final Dimension PANEL_MENU_SIZE = new Dimension(WINDOW_HEIGHT/2, WINDOW_WIDTH/2);
 
-	public static final int FPS = 120;
+	public static final int FPS =60;
 	public static final int REFRESH_RATE = 1000/FPS; // v ms, 1000/60 zabezpeci 60 FPS
 	
 	public static double playerSpeed = 5;
